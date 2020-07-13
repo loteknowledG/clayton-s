c1d915ceba7c6356797d4ab12df85662e5b8d306
loteknowledG <loteknowledG@gmail.com>
2020-07-13 07:57:52 -0400

    Updates

---
+++ precache-manifest.d6e7a2c6418a5f7391bef5a339587a87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2fb71f0129153520e632127b5807ce",
+    "url": "/clayton-s/index.html"
+  },
+  {
+    "revision": "bda4eeff560b65501746",
+    "url": "/clayton-s/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3643b44014412d59c0af",
+    "url": "/clayton-s/static/js/2.4a47fd33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clayton-s/static/js/2.4a47fd33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda4eeff560b65501746",
+    "url": "/clayton-s/static/js/main.1a03fd6d.chunk.js"
+  },
+  {
+    "revision": "09f68b99c561da018852",
+    "url": "/clayton-s/static/js/runtime-main.1965228a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clayton-s/static/media/logo.5d5d9eef.svg"
+  }
+]);
